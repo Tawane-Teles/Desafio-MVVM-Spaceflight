002d5db16ec28e92f3484e65c3f8770cb18f7ac5
Tawane Teles <73786592+Tawane-Teles@users.noreply.github.com>
2021-04-26 15:30:09 -0300

    Finalizando esqueleto.

--- app/src/main/java/com/spaceflight/di/Module.kt
+++ app/src/main/java/com/spaceflight/di/Module.kt
@@ -1,10 +1,20 @@
 package com.spaceflight.di
 
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.spaceflight.network.ApiService
+import com.spaceflight.utils.Constant.BASE_URL
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+val applicationModule = module {
+    single { providerHttpLoggingInterceptor() }
+    single { provideOkHttp(get()) }
+    single { provideRetrofit(get()) }
+
+}
 
 val viewModelModule = module {
 
@@ -19,6 +29,22 @@ private fun providerHttpLoggingInterceptor(): HttpLoggingInterceptor =
         level = HttpLoggingInterceptor.Level.BODY
     }
 
+private fun provideOkHttp(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+    val okHttpClient = OkHttpClient.Builder()
+    okHttpClient.apply {
+        addInterceptor(httpLoggingInterceptor)
+    }
+    return okHttpClient.build()
+}
+
+
 private fun provideRetrofit(okHttpClient: OkHttpClient): ApiService {
-     val retrofit = Retrofit
+    val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(CoroutineCallAdapterFactory())
+        .client(okHttpClient)
+        .build()
+
+    return retrofit.create(ApiService::class.java)
 }
