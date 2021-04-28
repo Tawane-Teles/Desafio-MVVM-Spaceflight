b5934cc51e8522f514635e6bacb5fb7792e8185b
Tawane Teles <73786592+Tawane-Teles@users.noreply.github.com>
2021-04-28 15:27:27 -0300

    finalizando view model e iniciando chamada

--- app/src/main/java/com/spaceflight/repository/NewsRepository.kt
+++ app/src/main/java/com/spaceflight/repository/NewsRepository.kt
@@ -4,7 +4,5 @@ import com.spaceflight.network.ApiService
 
 
 class NewsRepository(private val apiService: ApiService) {
-    suspend fun getNews(limit: Int, start: Int) = ApiService.getNews(limit, start)
-
-
+    suspend fun getNews(limit: Int, start: Int) = apiService.getNews(limit, start).await()
 }
--- app/src/main/java/com/spaceflight/ui/HomeViewModel.kt
+++ app/src/main/java/com/spaceflight/ui/HomeViewModel.kt
@@ -2,6 +2,29 @@ package com.spaceflight.ui
 
 import androidx.lifecycle.ViewModel
 import com.spaceflight.repository.NewsRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
-class HomeViewModel(val newsRepository: NewsRepository) : ViewModel() {
+class HomeViewModel(val repository: NewsRepository) : ViewModel(), CoroutineScope {
+
+    private val job = Job()
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.IO + job
+
+    fun getNews(){
+        launch {
+            try {
+                val response = repository.getNews(15, 1)
+                if (response.isSuccessful)
+
+            }catch (e: Exception){
+
+            }
+        }
+    }
+
+
 }
