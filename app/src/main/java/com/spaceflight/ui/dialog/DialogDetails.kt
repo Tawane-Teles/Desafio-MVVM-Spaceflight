be52764d9410c5c2ea2ffd3a01a94c8ee9875150
Tawane Teles <73786592+Tawane-Teles@users.noreply.github.com>
2021-05-04 11:33:16 -0300

    alterando botao close p/ full screen e inserindo live data

--- app/src/main/java/com/spaceflight/ui/dialog/DialogDetails.kt
+++ app/src/main/java/com/spaceflight/ui/dialog/DialogDetails.kt
@@ -5,14 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.Observer
+import com.bumptech.glide.Glide
+import com.spaceflight.R
 import com.spaceflight.databinding.FragmentDetailsBinding
+import com.spaceflight.network.response.NewsResponse
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class DialogDetails : DialogFragment(), DialogListener {
     lateinit var binding: FragmentDetailsBinding
     private val viewModel: DialogDetailsViewModel by viewModel()
 
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, R.style.FullScreenDialog)
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -30,6 +37,14 @@ class DialogDetails : DialogFragment(), DialogListener {
 
     private fun iniViewModel() {
         viewModel.listener = this
+        viewModel.getClick()
+
+        viewModel.news.observe(requireActivity(), Observer<NewsResponse>{
+            binding.textView.text = it.summary
+            Glide.with(binding.imageView2.context).load(it.imageUrl).into(binding.imageView2)
+
+
+        })
 
     }
 
